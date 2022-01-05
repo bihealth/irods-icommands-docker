65d66269e4d545b5d4d9c458095b899b6847ebff
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2022-01-05 12:01:20 +0100

    Small fixes.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -t ghcr.io/bihealth/irods-docker:4.2.11
-singularity build irods-icommands-4.2.11.sif docker://ghcr.io/bihealth/irods-docker:4.2.11
+docker build . -t ghcr.io/bihealth/irods-icommands-docker:4.2.11
+singularity build irods-icommands-4.2.11.sif docker://ghcr.io/bihealth/irods-icommands-docker:4.2.11
